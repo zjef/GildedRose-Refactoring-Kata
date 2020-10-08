7052817af9c2419e6f7526becb60bb94d0d713d2
zjef <zjeflaureyns@hotmail.com>
2020-10-08 15:27:08 +0200

    Extract constants

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,12 +1,16 @@
 package com.gildedrose
 
+const val AGED_BRIE = "Aged Brie"
+const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i].name != SULFURAS) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
@@ -14,7 +18,7 @@ class GildedRose(var items: Array<Item>) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].name == BACKSTAGE_PASSES) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -30,15 +34,15 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != SULFURAS) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != AGED_BRIE) {
+                    if (items[i].name != BACKSTAGE_PASSES) {
                         if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i].name != SULFURAS) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
